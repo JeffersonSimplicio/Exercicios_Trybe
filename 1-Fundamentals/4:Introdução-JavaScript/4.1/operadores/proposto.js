992116fef8a9d0854fb768b1b7641383c4589c1a
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-02-05 14:02:31 -0300

    Melhoramento do código  de operadores do dia 4.1

--- 1-Fundamentals/4:Introdução-JavaScript/4.1/operadores/proposto.js
+++ 1-Fundamentals/4:Introdução-JavaScript/4.1/operadores/proposto.js
@@ -2,6 +2,6 @@ const base = 5;
 let height = 8;
 
 let area = base * height;
-let perimetro = (base * 2) + (height * 2)
+let perimetro = (base + height) * 2;
 console.log('Área: '+area+'m².');
 console.log('Perimetro: '+perimetro+'m.');
