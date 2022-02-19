e787f08e65561df7c846ac24569144a044006b21
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-02-19 10:59:28 -0300

    Adição de controle de code do background, cor da fonte e tamanho da fonte

---
+++ 1-Fundamentals/5:JS:DOM-Eventos-Web_Storage/5.4/script.js
@@ -0,0 +1,60 @@
+window.onload = () => {
+  function setBackgroundColor(color) {
+    let body = document.querySelector('body');
+    body.style.backgroundColor = color;
+    localStorage.setItem('backgroundColor', color);
+  }
+
+  function setFontColor(color) {
+    let body = document.querySelector('body');
+    body.style.color = color;
+    localStorage.setItem('fontColor', color);
+  }
+
+  function setFontSize(size) {
+    let body = document.querySelector('body');
+    body.style.fontSize = size;
+    localStorage.setItem('fontSize', size);
+  }
+
+  // Mudança de background-color
+  let backgroundColorButton = document.querySelectorAll('#background-color>button');
+  for (let i = 0; i < backgroundColorButton.length; i += 1) {
+    backgroundColorButton[i].addEventListener('click', (event) => {
+      setBackgroundColor(event.target.value);
+    });
+  }
+
+  // Mudança da cor da fonte
+  let fontColorButton = document.querySelectorAll('#font-colo>button');
+  for (let i = 0; i < fontColorButton.length; i += 1) {
+    fontColorButton[i].addEventListener('click', (event) => {
+      setFontColor(event.target.value);
+    });
+  }
+
+  // Mudança do tamanho da fonte
+  let fontSizeButton = document.querySelectorAll('#font-size>button');
+  for (let i = 0; i < fontSizeButton.length; i += 1) {
+    fontSizeButton[i].addEventListener('click', (event) => {
+      setFontSize(event.target.innerText);
+    });
+  }
+
+  // Coloca as cores ao reiniciar a pagina
+  function startup() {
+    let backgroundColor = localStorage.getItem('backgroundColor');
+    if(backgroundColor !== null) {
+      setBackgroundColor(backgroundColor);
+    }
+    let fontColor = localStorage.getItem('fontColor');
+    if(fontColor !== null) {
+      setFontColor(fontColor);
+    }
+    let fontSize = localStorage.getItem('fontSize');
+    if(fontSize !== null) {
+      setFontSize(fontSize);
+    }
+  }
+  startup();
+}
