8dcd8ddf268836744c9ea09f7fae863d7f51bc55
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-03-22 22:15:10 -0300

    Exercicio 1 finalizado e ex2 em reta final

---
+++ 1-Fundamentals/9:JavaScript-e-Testes-Assíncronos/9.2/ex2/api.js
@@ -0,0 +1,26 @@
+const fetchCurrency = async () => {
+  const url = 'https://api.coincap.io/v2/assets';
+  
+  const currency = await fetch(url)
+    .then((response) => response.json())
+    .then((data) => data.data)
+    .catch((error) => `Eita! Algo deu errado: ${error}`)
+
+  return currency;
+};
+
+const setCurrency = async () => {
+  const arrayCurrency = await fetchCurrency();
+  const list = document.getElementById('currency-list');
+
+  arrayCurrency.forEach(object => {
+    const newLi = document.createElement('li');
+    const dollarValue = Number(object.priceUsd);
+    
+    newLi.innerText = `${object.name} (${object.symbol}): ${dollarValue.toFixed(2)}`;
+    list.appendChild(newLi);
+  });
+};
+
+
+window.onload = () => setCurrency();
