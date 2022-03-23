bd644321c2b46ce19e066d1ba7f14a44c242d90a
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-03-23 19:09:41 -0300

    Exercicio 2 do dia 8.1 concluido

---
+++ 1-Fundamentals/8:Higher-Order-Functions-JavaScript-ES6/8.1/ex02.js
@@ -0,0 +1,9 @@
+const numberCheck = (choseNumber, randoName) => choseNumber === randoName;
+
+const sortedNumber = (myNumber, callback) => {
+  const number = Math.floor((Math.random() * 5) + 1);
+  
+  return callback(myNumber, number) ? 'Lucky day, you won!' : 'Try Again!';
+};
+
+console.log(sortedNumber( 1 ,numberCheck));
