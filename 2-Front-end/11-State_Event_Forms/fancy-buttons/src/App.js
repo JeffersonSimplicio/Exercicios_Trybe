4612e3a6a6a6561c87587ffdc5db4bfe4e238981
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-04-07 19:31:52 -0300

    Funções movidas para dentro do componente

--- 2-Front-end/11-State_Event_Forms/fancy-buttons/src/App.js
+++ 2-Front-end/11-State_Event_Forms/fancy-buttons/src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 
-function clickedOne() {
-  console.log('Clicou no 1!')
-}
-
-function clickedTwo() {
-  console.log('Clicou no 2!')
-}
-
-function clickedThree() {
-  console.log('Clicou! no 3!')
-}
-
 class App extends React.Component {
+
+  clickedOne() {
+    console.log('Clicou no 1!');
+  };
+  
+  clickedTwo() {
+    console.log('Clicou no 2!');
+  };
+  
+  clickedThree() {
+    console.log('Clicou! no 3!');
+  };
+
   render() {
     return (
       <>
-        <button onClick={ clickedOne }>Clique Aqui!</button>
-        <button onClick={ clickedTwo }>Ou Aqui!</button>
-        <button onClick={ clickedThree }>Talvez Aqui!</button>
+        <button onClick={ this.clickedOne }>Clique Aqui!</button>
+        <button onClick={ this.clickedTwo }>Ou Aqui!</button>
+        <button onClick={ this.clickedThree }>Talvez Aqui!</button>
       </>
     )
   }
