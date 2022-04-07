3c32f9c0f935495244fa677c31545a30f5163620
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-04-07 19:37:27 -0300

    acesso ao objeto this na função

--- 2-Front-end/11-State_Event_Forms/fancy-buttons/src/App.js
+++ 2-Front-end/11-State_Event_Forms/fancy-buttons/src/App.js
@@ -2,24 +2,31 @@ import React from 'react';
 
 class App extends React.Component {
 
+  constructor() {
+    super();
+    this.clickedOne = this.clickedOne.bind(this);
+    this.clickedTwo = this.clickedTwo.bind(this);
+    this.clickedThree = this.clickedThree.bind(this);
+  }
+
   clickedOne() {
-    console.log('Clicou no 1!');
+    console.log('O this do 1 é:', this);
   };
   
   clickedTwo() {
-    console.log('Clicou no 2!');
+    console.log('O this do 2 é:', this);
   };
   
   clickedThree() {
-    console.log('Clicou! no 3!');
+    console.log('O this do 3 é:', this);
   };
 
   render() {
     return (
       <>
-        <button onClick={ this.clickedOne }>Clique Aqui!</button>
-        <button onClick={ this.clickedTwo }>Ou Aqui!</button>
-        <button onClick={ this.clickedThree }>Talvez Aqui!</button>
+        <button onClick={ this.clickedOne }>Botão 1!</button>
+        <button onClick={ this.clickedTwo }>Botão 2!</button>
+        <button onClick={ this.clickedThree }>Botão 3!</button>
       </>
     )
   }
